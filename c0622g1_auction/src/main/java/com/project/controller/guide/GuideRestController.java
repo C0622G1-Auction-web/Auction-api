9837578e69e1d2a3314f8739cca1ddc66af7a3b9
nguyenduyquang2712 <quangkg8@gmail.com>
2022-12-13 11:51:51 +0700

    update

--- c0622g1_auction/src/main/java/com/project/controller/guide/GuideRestController.java
+++ c0622g1_auction/src/main/java/com/project/controller/guide/GuideRestController.java
@@ -1,4 +1,26 @@
-package com.project.controller.guide;
+package com.project.controller;
 
+import com.project.model.guide.Guide;
+import com.project.service.guide.IGuideService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("auction/api/guide")
+@CrossOrigin("*")
 public class GuideRestController {
+    @Autowired
+    private IGuideService guideService;
+    @GetMapping()
+    public ResponseEntity<List<Guide>> getGuideList() {
+        List<Guide> guideList = guideService.findAll();
+        return new ResponseEntity<>(guideList, HttpStatus.OK);
+    }
 }
--- c0622g1_auction/src/main/java/com/project/service/guide/IGuideService.java
+++ c0622g1_auction/src/main/java/com/project/service/guide/IGuideService.java
@@ -1,4 +1,8 @@
 package com.project.service.guide;
+import com.project.model.guide.Guide;
+
+import java.util.List;
 
 public interface IGuideService {
+    List<Guide> findAll();
 }
