3a415a31aa1119b234aaa66be431a491ae2b4b24
unknown <hgno3001@gmail.com>
2022-12-14 22:33:01 +0700

    update feature after merge from GiangLBH HaiNT HuyNV

--- c0622g1_auction/src/main/java/com/project/controller/users/UserRestController.java
+++ c0622g1_auction/src/main/java/com/project/controller/users/UserRestController.java
@@ -1,12 +1,20 @@
 package com.project.controller.users;
 
+
+import com.project.dto.UserListDto;
 import com.project.dto.user.UserTopDto;
+import com.project.model.users.User;
+import com.project.service.account.IAccountService;
+import com.project.service.users.IAddressService;
 import com.project.service.users.IUserService;
+import com.project.service.users.IUserTypeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -17,21 +25,108 @@ public class UserRestController {
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private IAddressService addressService;
+    @Autowired
+    private IAccountService accountService;
+    @Autowired
+    private IUserTypeService userTypeService;
+
+    /**
+     * Create by: HaiNT
+     * Date created: 13/12/2022
+     *
+     * @param id
+     * @param name
+     * @param email
+     * @param address
+     * @param userTypeId
+     * @return List User by param if param is empty then return list all users
+     */
+    @GetMapping
+    public ResponseEntity<List<UserListDto>> getAllUser(
+            @RequestParam(defaultValue = "") String id,
+            @RequestParam(defaultValue = "") String name,
+            @RequestParam(defaultValue = "") String email,
+            @RequestParam(defaultValue = "") String address,
+            @RequestParam(defaultValue = "") String userTypeId
+    ) {
+        List<User> userList = userService.getUserBy(id, name, email, userTypeId, address);
+        if (userList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        List<UserListDto> userListDtos = new ArrayList<>();
+        for (User user : userList) {
+            UserListDto userListDto = new UserListDto();
+            BeanUtils.copyProperties(user, userListDto);
+            userListDto.setId(user.getId());
+            userListDtos.add(userListDto);
+        }
+        return new ResponseEntity<>(userListDtos, HttpStatus.OK);
+    }
+
+    /**
+     * Create by: HaiNT
+     * Date created: 13/12/2022
+     *
+     * @param id
+     * @return Object user by id
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<User> userById(@PathVariable() int id) {
+        User user = userService.findById(id).get();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    /**
+     * Create by: HaiNT
+     * Date created: 13/12/2022
+     *
+     * @param id
+     * @param userListDto
+     * @return the user object is updated
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<UserListDto> updateUser(@PathVariable() int id, @RequestBody UserListDto userListDto) {
+        User user = userService.findById(id).get();
+        BeanUtils.copyProperties(userListDto, user);
+        userService.updateUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * Create by: HaiNT
+     * Date created: 13/12/2022
+     *
+     * @param idList
+     * @return the user object is unlock
+     */
+    @PutMapping("/unlockUser")
+    public ResponseEntity<UserListDto> unlockUser(@RequestBody List<Integer> idList) {
+        List<User> userList = userService.findByIdList(idList);
+        if (idList.size() != userList.size()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        userService.unlockUser(idList);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
     /**
      * Created: SangDD
      * Created date: 13/12/2022
      * Function: get Top 10 users with the highest total money auction
-     * @return HttpStatus.OK if result is not empty
+     *
      * @return HttpStatus.NOT_FOUND if result is not empty
      */
     @GetMapping("/top/{quality}")
     public ResponseEntity<List<UserTopDto>> getTopAuctionUser(@PathVariable String quality) {
         String regexNumber = "^\\d+$";
-        if(!quality.matches(regexNumber)) {
+        if (!quality.matches(regexNumber)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         List<UserTopDto> userTopDtoList = userService.getTopAuctionUser(quality);
-        if(userTopDtoList.size() == 0) {
+        if (userTopDtoList.size() == 0) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(userTopDtoList, HttpStatus.OK);
