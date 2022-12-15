f5a6b8b9e973135c42a76edc2f1bd316ab95a5cf
minhtien100396 <minhtien100396@gmail.com>
2022-12-15 13:34:46 +0700

    TienBM-update validate of auction

--- c0622g1_auction/src/main/java/com/project/dto/AuctionDto.java
+++ c0622g1_auction/src/main/java/com/project/dto/AuctionDto.java
@@ -1,19 +1,36 @@
 package com.project.dto;
 
+import com.project.service.auction.IAuctionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
 import javax.validation.constraints.NotNull;
 
-public class AuctionDto {
+public class AuctionDto implements Validator {
+
+
     private Integer id;
-    @NotNull
+    @NotNull(message = "Current price can not be null")
     private Double currentPrice;
     private String auctionDay;
     private Integer userId;
     private Integer productId;
-//    private User user;
+    private Double maxCurrentPrice;
+    //    private User user;
 //    private Product product;
     private String fullName;
 
     public AuctionDto() {
+    }
+
+    public Double getMaxCurrentPrice() {
+        return maxCurrentPrice;
+    }
+
+    public void setMaxCurrentPrice(Double maxCurrentPrice) {
+        this.maxCurrentPrice = maxCurrentPrice;
     }
 
     public Integer getUserId() {
@@ -66,4 +83,21 @@ public class AuctionDto {
     }
 
 
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        AuctionDto auctionDto = (AuctionDto) target;
+        if (auctionDto.getCurrentPrice() <= 0) {
+            errors.rejectValue("currentPrice", "", "Current price must be positive");
+        }
+        if (auctionDto.getCurrentPrice() <= auctionDto.getMaxCurrentPrice()) {
+            errors.rejectValue("currentPrice", "", "Bid cannot be less than current price");
+        }
+
+
+    }
 }
