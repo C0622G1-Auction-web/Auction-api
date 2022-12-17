7c09dd7006494d62af86588368a8f72bd2d718b3
minhtien100396 <minhtien100396@gmail.com>
2022-12-17 19:05:37 +0700

    TienBM

--- c0622g1_auction/src/main/java/com/project/controller/auction/AuctionRestController.java
+++ c0622g1_auction/src/main/java/com/project/controller/auction/AuctionRestController.java
@@ -68,7 +68,7 @@ public class AuctionRestController {
      * @param pageable
      * @return HttpStatus.NO_CONTENT if result is empty or HttpStatus.OK if result is not empty
      */
-    @GetMapping("/tien/{productId}")
+    @GetMapping("/auction-product/{productId}")
     public ResponseEntity<Page<AuctionDto>> getPageAuctionByProductId(
             @PageableDefault(value = 3) Pageable pageable,
             @PathVariable(value = "productId") Integer productId) {
