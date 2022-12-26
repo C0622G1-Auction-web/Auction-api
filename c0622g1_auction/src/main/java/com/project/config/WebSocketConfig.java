f438b957d73c0b53a3a55e867c08647ce9ac5d59
trungdc-ha <dangchitrungha@gmail.com>
2022-12-26 14:12:05 +0700

    TrungDC update WebSocketConfig

--- c0622g1_auction/src/main/java/com/project/config/WebSocketConfig.java
+++ c0622g1_auction/src/main/java/com/project/config/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins(auction_domain).withSockJS();
+                .setAllowedOrigins("*").withSockJS();
     }
 
     @Override
