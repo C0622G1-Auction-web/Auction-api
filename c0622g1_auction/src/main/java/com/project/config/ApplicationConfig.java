f07f387770bcc32353ccd23154b861e4abecaeb9
trungdc-ha <dangchitrungha@gmail.com>
2022-12-26 15:25:01 +0700

    TrungDC update config CORS

--- c0622g1_auction/src/main/java/com/project/config/ApplicationConfig.java
+++ c0622g1_auction/src/main/java/com/project/config/ApplicationConfig.java
@@ -2,14 +2,32 @@ package com.project.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class ApplicationConfig {
+@Configuration
+@EnableWebMvc
+public class ApplicationConfig implements WebMvcConfigurer {
     @Bean
     public MessageSource messageSource(){
         ReloadableResourceBundleMessageSource messageSource=new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("messageValidate");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .maxAge(3600)
+                .allowedHeaders("Accept", "Content-Type", "Origin",
+                        "Authorization", "X-Auth-Token")
+                .exposedHeaders("X-Auth-Token", "Authorization")
+                .allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS");
     }
 }
